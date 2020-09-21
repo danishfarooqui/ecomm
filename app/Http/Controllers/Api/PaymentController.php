25163d65d9c8397a2866faaeefe15e54dcbb28b4
Danish <danishfarooqui99@yahoo.com>
2020-09-21 13:10:15 +0530

    stripe payment api added

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -31,23 +31,28 @@ class PaymentController extends Controller
     public function makePayment(Request $request){
         // Set your secret key. Remember to switch to your live secret key in production!
 // See your keys here: https://dashboard.stripe.com/account/apikeys
+
+
+        return $cartItems = $request->input('cartItems');
         \Stripe\Stripe::setApiKey('sk_test_51HTQxNLvujkiiWhM3qtdjSi9dlCopxS8gGY5HqRn3ZpP5xTeWG5pd8oms60Z4W0ysQm1QKaTDKDiKGi1vurQHYcF00vuYTv19J');
 
         $token = \Stripe\Token::create([
-           'card'=>[
-               'number' => $request->input('cardNumber'),
-               'exp_month'=>$request->input('expiryMonth'),
-               'exp_year'=>$request->input('expiryYear'),
-               'cvc'=>$request->input('cvcNumber')
-           ]
+            'card' => [
+                'number' => $request->input('cardNumber'),
+                'exp_month' => $request->input('expiryMonth'),
+                'exp_year' => $request->input('expiryYear'),
+                'cvc' => $request->input('cvcNumber')
+            ]
         ]);
 
-        \Stripe\PaymentIntent::create([
+        $charge = \Stripe\Charge::create([
             'amount' => 1000,
-            'currency' => 'inr',
-            'payment_method_types' => $token,
-            'receipt_email' => 'jenny.rosen@example.com',
+            'currency' => 'usd',
+            'source' => $token,
+            'receipt_email' => $request->input('email'),
         ]);
+
+        return $charge;
 
     }
     /**
